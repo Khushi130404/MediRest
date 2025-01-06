3a3a725e8351f699e98717e036399654c470cc2b
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-06 19:14:51 +0530

    Initial

---
+++ src/main/java/com/example/MediCure/MediCureApplication.java
@@ -0,0 +1,13 @@
+package com.example.MediCure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediCureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediCureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MediCure/MediCureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MediCure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediCureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
