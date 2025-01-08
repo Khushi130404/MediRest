fb5850748a646740263b4532c792e568fbfa2292
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-08 18:54:22 +0530

    logreg-ui

--- src/main/java/com/example/MediCure/resource/UserController.java
+++ src/main/java/com/example/MediCure/resource/UserController.java
@@ -1,4 +1,17 @@
 package com.example.MediCure.resource;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/user")
 public class UserController {
+
+    @GetMapping(value = "/")
+    public String getHomePage()
+    {
+        return "home";
+    }
+
 }
