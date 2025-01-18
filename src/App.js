40818ea0aa48dd0aa410be23bcc16ccd09f18c88
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-18 23:26:43 +0530

    Home UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Route path="/about" element={<h2>About Us</h2>} />
           <Route path="/appointment" element={<h2>Book Appointment</h2>} />
           <Route path="/profile" element={<h2>User Profile</h2>} />
-          <Route path="/register" element={<h2>Register</h2>} />
+          <Route path="/register" element={<RegisterForm></RegisterForm>} />
           <Route path="/login" element={<LoginForm></LoginForm>} />
         </Routes>
       </div>
