0d7df31bd93e109b4a29f09fd177de0eba86f460
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-22 14:15:13 +0530

    Cross Origin

--- src/main/java/com/example/MediCure/resource/AppointmentController.java
+++ src/main/java/com/example/MediCure/resource/AppointmentController.java
@@ -5,16 +5,18 @@ import com.example.MediCure.repository.AppointmentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/appointment")
-public class AppointmentController {
-
+public class AppointmentController
+{
     @Autowired
     AppointmentRepo ar;
 
