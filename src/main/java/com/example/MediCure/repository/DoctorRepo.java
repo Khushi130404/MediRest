001fa1d0a48bb144fbe42b1812979455fdffb2d5
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-29 14:58:58 +0530

    DoctorRepo_Update

--- src/main/java/com/example/MediCure/repository/DoctorRepo.java
+++ src/main/java/com/example/MediCure/repository/DoctorRepo.java
@@ -11,4 +11,9 @@ public interface DoctorRepo extends JpaRepository<DoctorInfo,Integer>
     public DoctorInfo findByDoctorMailAndDoctorPass(String mail, String pass);
 
     public DoctorInfo findByDoctorId(int docId);
+
+    @Transactional
+    @Modifying
+    @Query("update DoctorInfo di set di.doctorName = :name, di.doctorMail = :mail, di.doctorAge = :age, di.doctorMobile = :mobile, di.doctorAddress = :address, di.doctorGender = :gender, di.specialist = :specialist where di.doctorId = :doctorId")
+    public void updateDoctor(int doctorId, String name,String mail,int age,String mobile, String address, String gender,String specialist);
 }
