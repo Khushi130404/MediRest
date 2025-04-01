2a76a17d38b9428ca4d680975e822bda5f5691be
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-04-01 23:31:24 +0530

    data csv for ML model

---
+++ src/main/java/com/example/MediCure/resource/MLModelContoller.java
@@ -0,0 +1,17 @@
+package com.example.MediCure.resource;
+
+import org.springframework.web.bind.annotation.*;
+
+public class MLModelContoller
+{
+//    @GetMapping("/trainModel")
+//    public String trainModel() {
+//        predictionModel.trainModel();
+//        return "Model trained successfully!";
+//    }
+//
+//    @PostMapping("/predictSpecialist")
+//    public String predictSpecialist(@RequestBody double[] symptomVector) {
+//        return predictionModel.predictSpecialist(symptomVector);
+//    }
+}
