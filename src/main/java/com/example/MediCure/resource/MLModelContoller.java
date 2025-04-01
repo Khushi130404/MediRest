3e958f223a3f508bd707f0120089c8a246481c0a
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-04-01 23:47:17 +0530

    ML Controller

--- src/main/java/com/example/MediCure/resource/MLModelContoller.java
+++ src/main/java/com/example/MediCure/resource/MLModelContoller.java
@@ -1,17 +1,24 @@
 package com.example.MediCure.resource;
 
+import com.example.MediCure.service.SpecialistPredictionModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 public class MLModelContoller
 {
-//    @GetMapping("/trainModel")
-//    public String trainModel() {
-//        predictionModel.trainModel();
-//        return "Model trained successfully!";
-//    }
-//
-//    @PostMapping("/predictSpecialist")
-//    public String predictSpecialist(@RequestBody double[] symptomVector) {
-//        return predictionModel.predictSpecialist(symptomVector);
-//    }
+    @Autowired
+    private SpecialistPredictionModel predictionModel;
+
+    @GetMapping("/trainModel")
+    public String trainModel()
+    {
+        predictionModel.trainModel();
+        return "Model trained successfully!";
+    }
+
+    @PostMapping("/predictSpecialist")
+    public String predictSpecialist(@RequestBody double[] symptomVector)
+    {
+        return predictionModel.predictSpecialist(symptomVector);
+    }
 }
